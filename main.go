c9f2c845224e0f10e7269700946d478e2e4564cf
KallelGaNewk <kallelgn@gmail.com>
2023-04-29 01:25:59 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,158 @@
+// Build using:
+// > go build -o TIDAL-RPC.exe -ldflags -H=windowsgui
+// to build the app as GUI, and disable
+// the console.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/getlantern/systray"
+	"github.com/hugolgst/rich-go/client"
+)
+
+const (
+	LoopTimeout = 16 * time.Second
+	ClientID    = "1098099931006390325"
+)
+
+type Song struct {
+	Name   string
+	Artist string
+}
+
+func main() {
+	systray.Run(onReady, onExit)
+}
+
+func onReady() {
+	var RPCEnabled = true
+
+	err := client.Login(ClientID)
+	if err != nil {
+		panic(err)
+	}
+
+	iconBytes := getIconBytes()
+	systray.SetIcon(iconBytes)
+	titleMenu := systray.AddMenuItem("TIDAL Rich Presence for Discord", "TIDAL")
+	rpcToggle := systray.AddMenuItemCheckbox("Enable RPC", "RPC", RPCEnabled)
+	titleMenu.SetIcon(iconBytes)
+	systray.AddSeparator()
+	artistMenu := systray.AddMenuItem("Artist: None", "Artist")
+	songMenu := systray.AddMenuItem("Song name: None", "Name")
+	systray.AddSeparator()
+	quitItem := systray.AddMenuItem("Quit", "Quit the app, duh.")
+
+	go func() {
+		for {
+			tidalWindowName := getProcessWindowName("TIDAL")
+			song := formatSongName(tidalWindowName)
+
+			if song != nil {
+				songMenu.Show()
+				artistMenu.SetTitle(fmt.Sprintf("Artist: %s", song.Artist))
+				songMenu.SetTitle(fmt.Sprintf("Song name: %s", song.Name))
+
+				client.SetActivity(client.Activity{
+					Details:    song.Name,
+					State:      fmt.Sprintf("by %s", song.Artist),
+					LargeImage: "tidal",
+					LargeText:  "Listen to music the way it’s meant to sound.",
+				})
+			} else {
+				artistMenu.SetTitle("Not playing nothing")
+				songMenu.Hide()
+
+				client.SetActivity(client.Activity{
+					Details:    "Idling",
+					LargeImage: "tidal",
+					LargeText:  "Listen to music the way it’s meant to sound.",
+				})
+			}
+
+			time.Sleep(LoopTimeout)
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-rpcToggle.ClickedCh:
+				if rpcToggle.Checked() {
+					rpcToggle.Uncheck()
+					client.Logout()
+				} else {
+					rpcToggle.Check()
+					err := client.Login(ClientID)
+					if err != nil {
+						panic(err)
+					}
+				}
+
+				RPCEnabled = rpcToggle.Checked()
+			case <-quitItem.ClickedCh:
+				systray.Quit()
+				return
+			}
+		}
+	}()
+}
+
+func onExit() {
+	fmt.Println("Quitting")
+}
+
+func getIconBytes() []byte {
+	file, err := os.Open("icon.ico")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	iconBytes, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return iconBytes
+}
+
+func formatSongName(inputString string) *Song {
+	parts := strings.Split(inputString, " - ")
+	if len(parts) != 2 {
+		return nil
+	}
+
+	songName := strings.TrimSpace(parts[0])
+	songArtist := strings.TrimSpace(parts[1])
+
+	return &Song{
+		Name:   songName,
+		Artist: songArtist,
+	}
+}
+
+func getProcessWindowName(processName string) string {
+	pwsh_instance := exec.Command("powershell", fmt.Sprintf("(Get-Process %s).MainWindowTitle", processName))
+
+	// Hide PowerShell window from showing
+	// https://stackoverflow.com/a/48365926
+	pwsh_instance.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+
+	output, err := pwsh_instance.Output()
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return strings.TrimSpace(string(output))
+}
---
+++ readme.md
@@ -0,0 +1,53 @@
+# TIDAL Rich Presence for Discord
+
+This application provides TIDAL Rich Presence integration for Discord, allowing you to display your currently playing TIDAL song as your Discord status. It uses the TIDAL desktop client's window title to retrieve the currently playing song information and updates your Discord status accordingly.
+
+<small>Consider this as a humble project, as I'm still learning the language.</small>
+
+## Usage
+
+1. Run the built executable file (`TIDAL-RPC.exe`).
+2. The application will appear in your system tray.
+3. Right-click the application icon in the system tray to access the menu.
+4. By default, the Rich Presence integration is enabled.
+5. The menu options allow you to control the application and the Rich Presence feature:
+
+   - **Enable RPC**: Toggle the Rich Presence integration on or off.
+   - **Artist**: Shows the currently playing artist.
+   - **Song name**: Shows the name of the currently playing song.
+   - **Quit**: Exit the application.
+
+## Notes
+
+- **Currently, if the TIDAL app is minimized to the system tray, there is no active window available, which prevents the functionality from working at the moment.**
+- If no song is playing, the application sets your Discord status to "Idling."
+
+## Build Instructions
+
+To build the application, follow these steps:
+
+1. Ensure you have Go installed on your system.
+2. Open a terminal or command prompt.
+3. Navigate to the project directory.
+4. Run the following command to build the executable:
+
+```
+go build -o TIDAL-RPC.exe -ldflags -H=windowsgui
+```
+
+This command builds the application as a GUI and disables the console window.
+
+## Dependencies
+
+This application relies on the following dependencies:
+
+- [github.com/getlantern/systray](https://github.com/getlantern/systray): A cross-platform Go library for placing an icon and menu in the notification area.
+- [github.com/hugolgst/rich-go/client](https://github.com/hugolgst/rich-go/client): A Go library for Discord Rich Presence integration.
+
+## Contributing
+
+Contributions to this project are welcome. If you encounter any issues or have suggestions for improvements, please create an issue or a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
